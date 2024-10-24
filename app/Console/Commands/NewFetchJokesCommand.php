d0700d0b3bbbeb9a7d2416da4d7b76bc87e48975
vikaskumar0923 <mansukhsingh21@gmail.com>
2024-10-24 14:55:22 +0530

    final-update-command
    final-update-command

---
+++ app/Console/Commands/NewFetchJokesCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Joke;
+
+
+class NewFetchJokesCommand extends Command
+{
+    protected $signature = 'fetch:jokes';
+    protected $description = 'Fetch a random joke and store it in the database';
+
+    public function handle()
+    {
+        $response = Http::get('https://official-joke-api.appspot.com/random_joke');
+        $jokeData = $response->json();
+
+        if ($jokeData) {
+            Joke::create([
+                 'type' => $jokeData['type'],
+                'setup' => $jokeData['setup'],
+                'punchline' => $jokeData['punchline'],
+            ]);
+            $this->info('Joke saved successfully.');
+        } else {
+            $this->error('Failed to fetch joke.');
+        }
+    }
+}
