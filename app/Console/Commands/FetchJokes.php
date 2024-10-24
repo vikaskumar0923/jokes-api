a5bc23eb3d1af1f9a1e0c53dba9c377a18c9807e
vikaskumar0923 <mansukhsingh21@gmail.com>
2024-10-24 14:22:07 +0530

    command-created
    command-created

---
+++ app/Console/Commands/FetchJokes.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use App\Models\Joke;
+
+class FetchJokes extends Command
+{
+    protected $signature = 'jokes:fetch';
+    protected $description = 'Fetch a random joke and store it in the database';
+
+    public function handle()
+    {
+        $response = Http::get('https://official-joke-api.appspot.com/random_joke');
+        $jokeData = $response->json();
+
+        if ($jokeData) {
+            Joke::create([
+                 'type' => $jokeData['type'],
+                'setup' => $jokeData['setup'],
+                'punchline' => $jokeData['punchline'],
+            ]);
+            $this->info('Joke saved successfully.');
+        } else {
+            $this->error('Failed to fetch joke.');
+        }
+    }
+}
