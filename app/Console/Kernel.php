9689af48e55b9632ed717b669b59ae0a0b25ed74
vikaskumar0923 <mansukhsingh21@gmail.com>
2024-10-24 14:39:09 +0530

    new-update
    new-update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
 	 protected $commands = [
-        FetchJoke::class, // Register your command here
+        FetchJokes::class, // Register your command here
     ];
     protected function schedule(Schedule $schedule)
     {
